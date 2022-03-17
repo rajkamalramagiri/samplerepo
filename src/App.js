78adb309c98ea7ef77545e80800b4e9d7344425f
Surendra <v-surendra.raj@shutterfly.com>
2022-03-17 13:13:44 +0530

    flex and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react'
-import Counter from './Counter'
+import Bootstrap from './Bootstrap'
 import Flex from './Flex'
-import Form from './Form'
-import Json from './Json'
-import Map from './Map'
-import Review from './Review'
-import Vegetable from './Vegetable'
-import Voting from './Voting'
 
 class App extends Component {
   
   render() {
     return (
       <div>
-        <Voting title="C" />
-        <Voting title="C++" />
-        <Voting title="React"/>
-        <Voting title="Angular"/>
-        <Voting title="Java"/>
-        <Voting title="Phython"/>
-        
+        <Bootstrap/>  
         
       </div>
     )
--- src/Flex.js
+++ src/Flex.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
-
+import './flex.css'
  class Flex extends Component {
   render() {
     return (
-      <div>Flex</div>
+        <div className='container-flex'>
+            <div>Hello</div>
+            <h1>world</h1>
+            <h1>world</h1>
+
+            <div>welcome</div>
+      </div>
     )
   }
 }
