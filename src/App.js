df2a9d99794df2d055722c8edc9d9850476a9add
Surendra <v-surendra.raj@shutterfly.com>
2022-03-21 14:11:52 +0530

    toggling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import Bootstrap from './Bootstrap'
+import DayTen from './DayTen'
 import Flex from './Flex'
+import Form from './Form'
+import Toggle from './Toggle'
 
 class App extends Component {
   
   render() {
     return (
       <div>
-        <Bootstrap/>  
-        
+        <Toggle/>
       </div>
     )
   }
